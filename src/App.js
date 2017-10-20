36f2f00d7e0aa4fe5c3006b42e387f57ebbc49fd
Patricio Lopez <plopez@avalchile.cl>
2017-10-20 17:07:31 +0400

    login working

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
 import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers.js'
 
 // UI Components
-import LoginButtonContainer from './user/ui/loginbutton/LoginButtonContainer'
+import LoginButton from './user/ui/loginbutton/LoginButton'
 import LogoutButtonContainer from './user/ui/logoutbutton/LogoutButtonContainer'
 
 
@@ -104,11 +104,36 @@ var VineyardContainer = React.createClass({
   }
 });
 
+class Login extends Component {
+  render() {
+    return(
+      <main className="container">
+           <div class="text-center">
+           <p><img height="100px" src="https://s3.ap-south-1.amazonaws.com/demo-agrum/logo-fundo-branco.png" /></p>
+            <h1>Welcome to Agrum</h1>
+            <span><LoginButton /></span>
+            <br /><br />
+            <p><small>Developed by <strong>Daniel Novy's Team</strong> at Dubai's Hackaton</small></p>
+            <p><small>
+              <ul>
+                <li>Fernando Paris</li>
+                <li>Wesley van Heije</li>
+                <li>Luiz Hamilton Soares</li>
+                <li>Patricio L&oacute;pez</li>
+                <li>Marc de Klerk</li>
+              </ul>
+            </small></p>
+            </div>
+      </main>
+    )
+  }
+}
+
 render(
     <Router>
     <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="login" component={Home} />
+        <Route path="/login" component={Login} />
         <Route path="/vineyards" onEnter={requireAuth} component={VineyardContainer} />
         <Route path="/logged" component={Logged} />
     </Switch>
--- src/user/ui/loginbutton/LoginButton.js
+++ src/user/ui/loginbutton/LoginButton.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import Router from 'react-router-dom'
+import { Connect } from 'uport-connect'
+
+const uport = new Connect('Demo Agrum', {
+      clientId: '2ogNMLtfy6wRDFFDqcY7cJarDueAWALKNbv',
+      network: 'rinkeby'
+   //   signer: SimpleSigner('')
+    })
+
+export const web3 = uport.getWeb3()
 
 // Images
 import uPortLogo from '../../../img/uport-logo.svg'
 
 const LoginButton = ({ onLoginUserClick }) => {
   return(
-      <a href="#" className="btn btn-primary" onClick={(event) => onLoginUserClick(event)}><img height='25px' className="uport-logo" src={uPortLogo} alt="UPort Logo" />Login with UPort</a>
+      <a href="#" onClick={uPortLogin}>Login with UPort</a>
   )
+}
+
+function uPortLogin() {
+    uport.requestCredentials().then((credentials) => {
+    	window.location='/vineyards';
+    })
 }
 
 export default LoginButton
