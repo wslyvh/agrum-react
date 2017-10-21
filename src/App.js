c8f04c170ecafd193be2708433dbb1408ebcb1b9
Marc de Klerk <deklerkmc@gmail.com>
2017-10-21 09:56:40 +0400

    Prettify front page

--- src/App.js
+++ src/App.js
@@ -260,23 +260,21 @@ var VineyardContainer = React.createClass({
 class Login extends Component {
   render() {
     return(
-      <main className="container">
-           <div class="text-center">
-           <p><img height="100px" src="https://s3.ap-south-1.amazonaws.com/demo-agrum/logo-fundo-branco.png" /></p>
-            <h1>Welcome to Agrum</h1>
-            <span><LoginButton /></span>
-            <br /><br />
-            <p><small>Developed by <strong>Daniel Novy's Team</strong> at Dubai's Hackaton</small></p>
-            <p><small>
-              <ul>
-                <li>Fernando Paris</li>
-                <li>Wesley van Heije</li>
-                <li>Luiz Hamilton Soares</li>
-                <li>Patricio L&oacute;pez</li>
-                <li>Marc de Klerk</li>
-              </ul>
-            </small></p>
-            </div>
+      <main className="splash">
+         <img height="100px" src="https://s3.ap-south-1.amazonaws.com/demo-agrum/logo-fundo-branco.png" />
+          <h1>Welcome to Agrum</h1>
+          <span><LoginButton /></span>
+          <br />
+          <br />
+          <span>Developed by <strong>Daniel Novy's Team</strong> at Dubai's Hackaton</span>
+          <br />
+          <ul>
+            <li>Fernando Paris</li>
+            <li>Wesley van Heije</li>
+            <li>Luiz Hamilton Soares</li>
+            <li>Patricio L&oacute;pez</li>
+            <li>Marc de Klerk</li>
+          </ul>
       </main>
     )
   }
